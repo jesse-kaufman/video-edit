fee81c1be2bf048963d606b9868cba54c07dc8ff
Jesse Kaufman <jesse@jessekaufman.com>
2024-11-16 17:34:15 -0600

    Add VideoStream typedef

---
+++ src/@types/video-stream.js
@@ -0,0 +1,12 @@
+/**
+ * @typedef VideoStream
+ * @property {string} lang - Language code of the video stream.
+ * @property {string} codecName - Codec name of video stream.
+ * @property {string} codecLongName - Long name of codec.
+ * @property {string} [profile] - Codec profile.
+ * @property {number} index - Index of video stream.
+ * @property {string} title - Title of video stream.
+ * @property {string} resolution - Resolution of video stream.
+ */
+
+export default {};
