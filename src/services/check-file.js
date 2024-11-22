0ecb4cbfa5aa55529cc19f1be3cfc9a5f4a0a870
jesse-kaufman <jesse@jessekaufman.com>
2024-11-22 09:03:49 -0600

    Break out check for needs attention to separate service

---
+++ src/services/check-file.js
@@ -0,0 +1,28 @@
+/**
+ * @file Check file service.
+ */
+
+import { textSubTypes } from "./stream/subtitle-stream.js"
+
+/**
+ * Returns true if the stream needs attention.
+ * Stream needs attention if:
+ * - Language is not English.
+ * - Codec is not AAC (if audio track).
+ * - Codec is SubRip (if subtitle).
+ * @param {string} lang - Language of stream.
+ * @param {string} codec - Codec name of stream.
+ * @param {string} type - Type of stream (audio or subtitle).
+ * @returns {boolean} True if stream needs attention.
+ */
+export const streamNeedsAttention = (lang, codec, type) =>
+  lang !== "eng" ||
+  (type === "audio" && codec !== "aac") ||
+  (type === "subtitle" && textSubTypes.includes(codec))
+
+/**
+ * Returns true if the container needs attention (it is not MKV).
+ * @param {string} container - Container type of input file.
+ * @returns {boolean} True if container needs attention, otherwise false.
+ */
+export const containerNeedsAttention = (container) => container !== "MKV"
