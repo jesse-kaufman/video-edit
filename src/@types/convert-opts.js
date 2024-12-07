ae80b7b60cda1c24aecc5d6fbc86fb38fd4d3cc4
jesse-kaufman <jesse@jessekaufman.com>
2024-12-06 21:01:16 -0600

    Add forceConvert to options list

--- src/@types/convert-opts.js
+++ src/@types/convert-opts.js
@@ -4,6 +4,7 @@
  * @typedef {object} ConvertOpts
  * @property {boolean} [convertAudio] - Whether or not to convert audio streams.
  * @property {boolean} [convertVideo] - Whether or not to convert video stream.
+ * @property {boolean} [forceConvert] - Whether or not to force conversion of video.
  * @property {boolean} [extractSubs] - Whether or not to extract subtitles.
  * @property {boolean} [extractOnly] - Whether or not to only extract subtitles and skip the rest.
  */
